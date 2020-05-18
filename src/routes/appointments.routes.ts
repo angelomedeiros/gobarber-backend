966e22b5345db06371b48e1c77affdba774f0a4d
Angelo Medeiros <angelo.medeiros@linkle.io>
2020-05-18 16:18:34 -0300

    Cadastro de agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
 appointmentsRouter.post('/', (req, res) => {
-  return res.json(req.body);
+  const { provider, date } = req.body;
+
+  const appointment = {
+    provider,
+    date,
+    id: uuid(),
+  };
+
+  appointments.push(appointment);
+
+  return res.json(appointment);
 });
 
 export default appointmentsRouter;
