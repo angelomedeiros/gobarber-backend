0ee68351935e49c53d9dc252466ee071667ed198
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-05-17 23:49:55 -0300

    Add config do eslint para os imports

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello world!' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Listing port 3333 ⌚');
