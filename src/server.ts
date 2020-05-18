776de56cdcb7138c9b7e79069419895510019f99
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-05-17 23:42:27 -0300

    Configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Hello world!" }));
+app.get('/', (req, res) => res.json({ message: 'Hello world!' }));
 
 app.listen(3333, () => {
-  console.log(`Listing port 3333 ⌚`);
+  console.log('Listing port 3333 ⌚');
 });
