3b2e20d89382e3bc644f265bd9b39e674fbbb36a
Angelo Medeiros <angelo.medeiros@linkle.io>
2020-05-19 01:00:14 -0300

    Criando repository de appointment

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,25 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+export default class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    return (
+      this.appointments.find(appointment => isEqual(appointment.date, date)) ||
+      null
+    );
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express';
-import { parseISO, startOfHour, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { parseISO, startOfHour } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -20,9 +19,7 @@ appointmentsRouter.post('/', (req, res) => {
     });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
