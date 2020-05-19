400adaadd1deb9ca16ae4271996b4e8105b84c6f
Angelo Medeiros <angelo.medeiros@linkle.io>
2020-05-18 21:26:42 -0300

    Validando a data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { parseISO, startOfHour, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res.status(400).json({
+      message: 'This appointment is aleready booked',
+    });
+  }
+
   const appointment = {
     provider,
-    date,
+    date: parsedDate,
     id: uuid(),
   };
 
