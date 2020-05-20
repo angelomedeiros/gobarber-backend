34cc111a962bd044287809fa38d610a5472789a6
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-05-20 17:47:29 -0300

    Criando conecxao com o banco de dados

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
-app.get("/", function (req, res) { return res.json({ message: "Hello world!" }); });
-app.listen(3333, function () {
-    console.log("Listing port 3333 \u231A");
-});
