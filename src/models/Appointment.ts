96abb55073e3a1bd78592fe83b31a8b58ad44684
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-05-20 21:16:56 -0300

    Criando model de agendamento com o typeORM

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,15 +1,13 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 export default class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
