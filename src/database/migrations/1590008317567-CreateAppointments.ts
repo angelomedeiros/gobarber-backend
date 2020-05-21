ec87bd2481b3887ba1e4ee64a5578c2a5055f1d4
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-05-21 13:54:45 -0300

    Criandio relacionamento entre appointment e users

--- src/database/migrations/1590008317567-CreateAppointments.ts
+++ src/database/migrations/1590008317567-CreateAppointments.ts
@@ -9,14 +9,14 @@ export default class CreateAppointments1590008317567
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
-            type: 'varchar',
+            type: 'uuid',
             isNullable: false,
           },
           {
