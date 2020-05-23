943e22359438e67dbf7ea469c77c37fb74e62f79
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-05-23 17:07:17 -0300

    Criando rota para visualizar as imagens

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
+import { filesDirectory } from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(filesDirectory));
 app.use(routes);
 
 app.listen(3333, () => {
